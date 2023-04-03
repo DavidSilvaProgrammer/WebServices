827f6062dd5092e5eb670ab769aacedda0109384
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-04-03 18:19:05 -0300

    Delete gameRoutes.js

--- M4/Ativ2/gameRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const gameController = require('../controllers/gameController');
-
-// Cria um router para as rotas de games
-const router = express.Router();
-
-// Rota para criar um novo game
-router.post('/', gameController.criar
